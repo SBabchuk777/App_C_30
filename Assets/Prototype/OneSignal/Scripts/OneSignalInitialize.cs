e468d7822fab849ec0d9f9ccc350397bfa313374
SBabchuk777 <ba.s@dcjudgement.com>
2023-03-21 15:20:19 +0200

    Add parans for build by Ios

---
+++ Assets/Prototype/OneSignal/Scripts/OneSignalInitialize.cs
@@ -0,0 +1,25 @@
+using OneSignalSDK;
+using UnityEngine;
+
+public class OneSignalInitialize : MonoBehaviour
+{
+    [SerializeField] private string appIdIOS;
+    
+    [SerializeField] private string appIdAndroid;
+    private void Start () 
+    {
+#if UNITY_ANDROID
+        Debug.Log($"Initializing with appId <b>{appIdAndroid}</b>");
+        
+        OneSignal.Default.Initialize(appIdAndroid);
+#elif UNITY_IOS
+        Debug.Log($"Initializing with appId <b>{appIdIOS}</b>");
+        
+        OneSignal.Default.Initialize(appIdIOS);
+#endif
+        Debug.Log($"Initializing with appId <b>{appIdAndroid}</b>");
+        
+        OneSignal.Default.Initialize(appIdAndroid);
+       
+    }
+}
